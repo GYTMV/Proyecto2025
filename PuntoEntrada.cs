850be814cf3b2891719d0495089190a5c5e88d7b
EzeVelazquez <ezequiel.next.developer@gmail.com>
2025-11-12 22:21:44 -0300

    Cambios V0.5

--- PuntoEntrada.cs
+++ PuntoEntrada.cs
@@ -6,35 +6,64 @@ namespace practica2025
 {
     class PuntoEntrada
     {
-       
-        public string Nombre { get; set; }
-        public int PotenciaMaxima { get; set; }
-        public string URL { get; set; }
-        public string NombreDirector { get; set; }
-        public string TelDirector { get; set; }
-        public string NodoConexion { get; set; }
+        //Atributos de clase
+        public string nombre, url, nombreDirector, telDirector, nodoConexion;
+        public byte potenciaMaxima;
 
-        public PuntoEntrada()
-        {
-            this.Nombre = "Punto Default";
-            this.PotenciaMaxima = 100;
-            this.URL = "http://default.url";
-            this.NombreDirector = "Director Default";
-            this.TelDirector = "000-000-0000";
-            this.NodoConexion = "Nodo Default"; 
-
-        }
-
+        //Constructor 1
         public PuntoEntrada(string nombre, int PotenciaMaxima, string url, string nombreDirector, string telDirector, string nodoConexion)
         {
-            this.Nombre = nombre;
-            this.PotenciaMaxima = PotenciaMaxima;
-            this.URL = url;
-            this.NombreDirector = nombreDirector;
-            this.TelDirector = telDirector;
-            this.NodoConexion = nodoConexion;
+            this.nombre = nombre;
+            this.potenciaMaxima = PotenciaMaxima;
+            this.uRL = url;
+            this.nombreDirector = nombreDirector;
+            this.telDirector = telDirector;
+            this.nodoConexion = nodoConexion;
         }
 
+        //Constructor 2
+        public PuntoEntrada()
+        {
+            this.nombre = "Punto Default";
+            this.potenciaMaxima = 100;
+            this.url = "http://default.url";
+            this.nombreDirector = "Director Default";
+            this.telDirector = "000-000-0000";
+            this.nodoConexion = "Nodo Default";
+
+        }
+        public string GetNombre
+        {
+            get {return nombre;}
+        }
+
+        public int GetPotencia
+        {
+            get {return potenciaMaxima;}
+        }
+
+        public int GetUrl
+        {
+            get {return url;}
+        }
+
+        public string GetNombreDirector
+        {
+            get {return nombreDirector;}
+            set {nombreDirector = value;}
+        }
+
+        public string GetTelDirector
+        {
+            get {return telDirector;}
+            set {telDirector = value;}
+        }
+
+        public string GetNodoConexion
+        {
+            get {return nodoConexion;}
+        }
+            
 
         public void MostarInfo()
         {
@@ -43,6 +72,22 @@ namespace practica2025
             Console.WriteLine($"El nombre del director es {NombreDirector} y su telefono es {TelDirector}.");
             Console.WriteLine($"El nodo de conexion es {NodoConexion}.");
         }
+        public void MostrarInfoRed()
+        {
+            Console.WriteLine($"Hola, el nombre del punto es {Nombre} y su potencia maxima es {PotenciaMaxima}, El URL del punto de entrada es {URL} ");
+        }
+
+        public void InfoDirector()
+        {
+            Console.WriteLine($"El nombre del director es {NombreDirector} y su telefono es {TelDirector}.");
+
+        }
+
+        public void NodoConexion
+        {
+            Console.WriteLine($"El nodo de conexion es {NodoConexion}.");
+        }
+
         public static bool IsValidInteger(string input)
         {
             return !string.IsNullOrWhiteSpace(input) && int.TryParse(input, out _);
@@ -77,12 +122,12 @@ namespace practica2025
             string nodoConexion = Console.ReadLine()!;
 
             // Asignar los valores ingresados a las propiedades del objeto
-            this.Nombre = nombre;
-            this.PotenciaMaxima = potenciaMaxima;
-            this.URL = url;
-            this.NombreDirector = nombreDirector;
-            this.TelDirector = telDirector;
-            this.NodoConexion = nodoConexion;
+            this.nombre = nombre;
+            this.potenciaMaxima = potenciaMaxima;
+            this.url = url;
+            this.nombreDirector = nombreDirector;
+            this.telDirector = telDirector;
+            this.nodoConexion = nodoConexion;
 
         }
     }
